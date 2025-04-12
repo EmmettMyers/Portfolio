d189b2493e0d129623b19d68a49aab54ffa76d59
Emmett Myers <emmettleemyers@gmail.com>
2025-04-12 17:17:24 -0500

    Revert "Depreciation padding"
    This reverts commit 9660f2421b79c1743c8e4cadd37a153cd68051b4.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,7 +12,7 @@
     <!-- preloading images -->
     <img style="display: none;" @load="onImageLoad" v-for="imageSrc in imageSources" :key="imageSrc" :src="imageSrc">
 
-    <div v-if="imagesLoaded" class="sm:pt-4 pt-16">
+    <div v-if="imagesLoaded">
       <EducationModal v-if="educationModalOpen" />
       <ExperienceModal v-if="experienceModalOpen" />
       <ProjectsModal v-if="projectsModalOpen" />
