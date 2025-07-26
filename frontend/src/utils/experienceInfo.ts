8fd6e99b9db281adbb073474715dec6e4d4c18c3
Emmett Myers <emmettleemyers@gmail.com>
2025-07-26 13:31:53 -0700

    Update experienceInfo.ts

--- frontend/src/utils/experienceInfo.ts
+++ frontend/src/utils/experienceInfo.ts
@@ -38,7 +38,7 @@ export const experienceInfo = [
             "Designed capabilities API endpoints and routers to eliminate inefficient middleware",
             "Executed rigorous parameterized testing plans on all new services"
         ],
-        skills: ['JavaScript', 'Java', 'XPlat', 'Wiz', 'WebSockets', 'Pub/Sub', 'Caches', 'JUnit']
+        skills: ['JavaScript', 'Java', 'XPlat', 'Wiz', 'JUnit']
     },
     {
         title: 'Bosch',
